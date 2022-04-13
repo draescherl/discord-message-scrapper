d44622f4a20097665f437bbc9f66180b44c1360a
Lucas DRAESCHER <draescherl@cy-tech.fr>
2022-04-13 17:37:46 +0100

    Added colouring and error handling.

--- app.py
+++ app.py
@@ -1,35 +1,42 @@
 import json
 import os
 import discord
+from termcolor import colored
 
 
 async def get_all_messages():
-    channel = client.get_channel(466696591399714822)
-    messages = {}
-    count = 0
-    print(f"Reading messages of channel \"{channel.name}\" in server \"{channel.guild.name}\" ... ", end='')
-    async for message in channel.history(limit=None, oldest_first=True):
-        messages[count] = {
-            "contents":  message.content,
-            "author":    message.author.name,
-            "timestamp": message.created_at
-        }
-        count += 1
-    print("Done.")
-    filename = "./message_dump.json"
-    print(f"Writing {count + 1} messages to \"{filename}\" ... ", end='')
-    with open(filename, 'w') as outfile:
-        json.dump(messages, outfile, default=str, ensure_ascii=False, indent=2)
-    print("Done.")
+    try:
+        channel = client.get_channel(int(os.environ.get('CHANNEL_ID')))
+        messages = {}
+        count = 0
+        print(f'Reading messages of channel {colored(channel.name, "cyan")} in server {colored(channel.guild.name, "cyan")} ... ', end='')
+        async for message in channel.history(limit=None, oldest_first=True):
+            messages[count] = {
+                'contents':  message.content,
+                'author':    message.author.name,
+                'timestamp': message.created_at
+            }
+            count += 1
+        print(colored('Done.', 'green'))
+        filename = './message_dump.json'
+        print(f'Writing {colored(str(count + 1), "cyan")} messages to {colored(filename, "cyan")} ... ', end='')
+        with open(filename, 'w') as outfile:
+            json.dump(messages, outfile, default=str, ensure_ascii=False, indent=2)
+        print(colored('Done.', 'green'))
+    except AttributeError:
+        print(colored('The provided channel ID is either invalid or your bot does not have access to it.', 'red'))
 
 
 class Bot(discord.Client):
     async def on_ready(self):
-        print('Logged on as', self.user)
+        print(f'Logged on as {colored(self.user, "cyan")}.')
         await get_all_messages()
         await self.close()
 
 
 intents = discord.Intents.all()
 client = Bot(intents=intents)
-client.run(os.environ.get("BOT_TOKEN"))
+try:
+    client.run(os.environ.get('BOT_TOKEN'))
+except discord.errors.LoginFailure:
+    print(colored('The provided token is invalid.', 'red'))
