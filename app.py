49575d6c78d4ac7c5daded8f865ded2ccbb58ea7
Lucas DRAESCHER <draescherl@cy-tech.fr>
2022-04-13 17:15:51 +0100

    Added logging.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ async def get_all_messages():
     channel = client.get_channel(466696591399714822)
     messages = {}
     count = 0
+    print(f"Reading messages of channel \"{channel.name}\" in server \"{channel.guild.name}\" ... ", end='')
     async for message in channel.history(limit=None, oldest_first=True):
         messages[count] = {
             "contents":  message.content,
@@ -14,8 +15,9 @@ async def get_all_messages():
             "timestamp": message.created_at
         }
         count += 1
+    print("Done.")
     filename = "./message_dump.json"
-    print(f"Writing {count + 1} messages to {filename}")
+    print(f"Writing {count + 1} messages to \"{filename}\" ... ", end='')
     with open(filename, 'w') as outfile:
         json.dump(messages, outfile, default=str, ensure_ascii=False, indent=2)
     print("Done.")
@@ -23,11 +25,11 @@ async def get_all_messages():
 
 class Bot(discord.Client):
     async def on_ready(self):
+        print('Logged on as', self.user)
         await get_all_messages()
         await self.close()
 
 
 intents = discord.Intents.all()
 client = Bot(intents=intents)
-
 client.run(os.environ.get("BOT_TOKEN"))
